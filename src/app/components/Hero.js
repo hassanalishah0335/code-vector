21222d327a9e62c868a26915ac5cfe2b94759580
hassanalishah0335 <hassanalishah0335@gmail.com>
2024-07-13 22:02:54 +0500

    Updates

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => {
                 <source src="/resource/videos/video-1.mp4" type="video/mp4" />
             </video>
             <div className='absolute inset-0 flex flex-col ml-[5%] sm:top-[30%] top-[40%] items-start'>
-                <TypeAnimation sequence={["CodeVector Technologies", 1500, props.title, 2000, "Looking For A business Solution?", 2000, "Want to Develop A Web Or Software?", 2000]} wrapper="span" speed={20} repeat={Infinity} className='text-gray-100 font-semibold sm:w-[40%] text-4xl md:text-5xl lg:text-6xl xl:text-7xl sm:h-[50%] h-20' />
+                <TypeAnimation sequence={["CodeVector Technologies", 1500, props.title, 2000, "Looking For A business Solution?", 2000, "Want to Develop A Website?", 2000]} wrapper="span" speed={20} repeat={Infinity} className='text-gray-100 font-semibold sm:w-[40%] text-4xl md:text-5xl lg:text-6xl xl:text-7xl sm:h-[50%] h-20' />
                 <div className="flex">
                 <ScrollLink to="contact" spy={true} smooth={true} offset={-70} duration={500} className='bg-black opacity-60 flex justify-center items-center cursor-pointer text-xl md:w-40 md:h-16 sm:w-44 sm:h-20 w-40 h-14 mr-3 mt-3 italic font-semibold rounded-xl hover:opacity-90 active:opacity-100 text-white' >Get started</ScrollLink>
                 <ScrollLink to="footer" spy={true} smooth={true} offset={-70} duration={500} className='bg-white opacity-60 flex justify-center items-center cursor-pointer text-xl md:w-40 md:h-16 sm:w-44 sm:h-20 w-40 h-14 mr-3 mt-3 italic font-semibold  rounded-xl hover:opacity-90 active:opacity-100 text-black' >Contact Us</ScrollLink>
